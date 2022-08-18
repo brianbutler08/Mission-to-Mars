b08234b548dd17b59d4490df11e49eec1aacab62
Brian Butler <106709111+brianbutler08@users.noreply.github.com>
2022-08-18 15:17:27 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+# set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# set up route for html page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index (1).html", mars=mars)
+
+# set up scraping route
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
+
+# tell flask to run
+if __name__ == "__main__":
+   app.run()
